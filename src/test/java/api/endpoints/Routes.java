c0f3a874182c3f42194573b1c12da9e3fe496573
SoftwareTestingByRajTripathi <tripathideva@gmail.com>
2023-08-31 15:12:50 +0530

    Framework Code is Update by raj 31082023

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,16 @@
+package api.endpoints;
+
+public class Routes {
+
+	public static String BASE_URL = "https://petstore.swagger.io/v2";
+
+	//User Model//
+	public static String POST_URL = BASE_URL+"/user";
+	public static String GET_URL = BASE_URL+"/user/{username}";
+	public static String UPDATE_URL = BASE_URL+"/user/{username}";
+	public static String DELETE_URL = BASE_URL+"/user/{username}";
+	
+	//Pet Model
+	// Write the URI of Pet Model//
+
+}
