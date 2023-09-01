5704c1305103173d02aaeff75c4b319ebf1e0082
SoftwareTestingByRajTripathi <tripathideva@gmail.com>
2023-09-01 15:39:55 +0530

    update xl utility and Data provider code

--- src/test/java/api/utilities/DataProviders.java
+++ src/test/java/api/utilities/DataProviders.java
@@ -2,10 +2,12 @@ package api.utilities;
 
 import java.io.IOException;
 
+import org.testng.annotations.DataProvider;
+
 public class DataProviders {
-	
+	@DataProvider(name ="Data")
 	public String[][] getAllData() throws IOException{
-		String path = System.getProperty("user.dir")+"//UserData.xlsx";
+		String path = System.getProperty("user.dir")+"//testData//UserData.xlsx";
 		XLUtility xl = new XLUtility(path);
 		
 		int rownum = xl.getRowCount("Sheet1");
@@ -18,6 +20,20 @@ public class DataProviders {
 		}
 		return apidata;
 		
+	}
+	@DataProvider(name ="UserNames")
+	public String [] getUserNames() throws IOException {
+		String path = System.getProperty("user.dir")+"//testData//UserData.xlsx";
+		XLUtility xl = new XLUtility(path);
+		
+		int rownum = xl.getRowCount("Sheet1");
+		
+		String apidata[] = new String[rownum];
+		
+		for(int i =1;i<=rownum;i++) {
+			apidata[i-1] = xl.getCellData("Sheet1", i, 1);
+		}
+		return apidata;
 	}
 
 }
